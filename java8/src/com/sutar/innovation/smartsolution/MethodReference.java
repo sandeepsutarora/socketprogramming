c905c35a2eb8dfac76a3c8bc146a36d86b38325e
Sandeep Sutar <sandeepsutarora@gmail.com>
2019-09-30 10:06:58 +0530

    first commit

--- java8/src/com/sutar/innovation/smartsolution/MethodReference.java
+++ java8/src/com/sutar/innovation/smartsolution/MethodReference.java
@@ -1,4 +1,29 @@
 package com.sutar.innovation.smartsolution;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.BiPredicate;
+
+class Numbers{
+    public static boolean isMoreThanFifty(int n1, int n2)
+    {
+        return (n1 + n2 ) > 50;
+
+    }
+
+    public static List<Integer> findNumbers(List<Integer> list, BiPredicate<Integer, Integer> p)
+    {
+        List<Integer> newList = new ArrayList<>();
+        for( Integer i: list)
+        {
+            if( p.test(i, i+10))
+                newList.add(i);
+        }
+        return newList;
+    }
+
+}
+
+
 public class MethodReference {
 }
